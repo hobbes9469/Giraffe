86c930549c5d7587f35c0328ca14820d1f7efae5
hobbes9469 <mattkang202@gmail.com>
2018-09-30 09:01:44 -0700

    Update giraffe.lisp

--- giraffe.lisp
+++ giraffe.lisp
@@ -5,27 +5,30 @@
 
 
 (defun pm (x y)    ; Possible Moves
-  (let ((a '())))
-  (setq movelist (list
-                  (list (+ x 1) y)  ; 4 1-space moves
-                  (list (- x 1) y)
-                  (list x (+ y 1))
-                  (list x (- y 1))
-                  (list (- x 1) (- y 2))  ; 8 knight moves
-                  (list (+ x 1) (- y 2))
-                  (list (+ x 2) (- y 1))
-                  (list (+ x 2) (+ y 1))
-                  (list (+ x 1) (+ y 2))
-                  (list (- x 1) (+ y 2))
-                  (list (- x 2) (+ y 1))
-                  (list (- x 2) (- y 1))
-                  ) )
-  (loop for x in movelist
-        do (if (vm (nth 0 x) (nth 1 x))
-               (format t  "(~D, ~D)~%" (nth 0 x) (nth 1 x))
-               (format t  "BANNED MOVE:(~D, ~D)~%" (nth 0 x) (nth 1 x))
-               )
-        )
+  (let ((a '()))
+    (setq movelist (list
+                    (list (+ x 1) y)  ; 4 1-space moves
+                    (list (- x 1) y)
+                    (list x (+ y 1))
+                    (list x (- y 1))
+                    (list (- x 1) (- y 2))  ; 8 knight moves
+                    (list (+ x 1) (- y 2))
+                    (list (+ x 2) (- y 1))
+                    (list (+ x 2) (+ y 1))
+                    (list (+ x 1) (+ y 2))
+                    (list (- x 1) (+ y 2))
+                    (list (- x 2) (+ y 1))
+                    (list (- x 2) (- y 1))
+                    ) )
+    (loop for x in movelist  ; TODO: change the if to append to list a
+          do (if (vm (nth 0 x) (nth 1 x))
+                 (setq a (append a (list x)))
+                ;(format t  "(~D, ~D)~%" (nth 0 x) (nth 1 x))
+                 (format t  "BANNED MOVE:(~D, ~D)~%" (nth 0 x) (nth 1 x))
+                 )
+          )
+    (print a)
+    )
   )
 
 (pm 0 0)
