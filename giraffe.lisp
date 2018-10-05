64f33fe63c07d17e74541cc7984173e9f9c4e3c0
hobbes9469 <mattkang202@gmail.com>
2018-10-05 16:40:46 -0700

    Update giraffe.lisp

--- giraffe.lisp
+++ giraffe.lisp
@@ -1,3 +1,6 @@
+(defvar movelist)
+(defvar b)
+
 (defun vm (x y)    ; Valid Moves
   (if (and (<= x 6) (>= x 0) (<= y 6) (>= y 0)) 
       t nil)   ; If x and y between 0 and 6, valid.
@@ -20,11 +23,9 @@
                     (list (- x 2) (+ y 1))
                     (list (- x 2) (- y 1))
                     ) )
-    (loop for x in movelist  ; TODO: change the if to append to list a
+    (loop for x in movelist  
           do (if (vm (nth 0 x) (nth 1 x))
                  (setq a (append a (list x)))
-                ;(format t  "(~D, ~D)~%" (nth 0 x) (nth 1 x))
-                 ;(format t  "BANNED MOVE:(~D, ~D)~%" (nth 0 x) (nth 1 x))
                  )
           )
     a 
@@ -35,19 +36,25 @@
 
 
 (defun gir (L)
-  (format t "CURRENT MOVELIST (L): ~S" L)
-  (format t "~%POSSIBLE MOVES: ~S~%" (pm (car (car L)) (car (cdr (car L)))))
+  ;(format t "CURRENT MOVELIST (L): ~S" L)
+  ;(format t "~%POSSIBLE MOVES: ~S~%" (pm (car (car L)) (car (cdr (car L)))))
   (if (= (list-length L) 49)
+      (format t "L: ~S~%" L) )      ; Print out the current path if length is 49
+  (if (= (list-length L) 49)   ; If the path is length 49, return it
       (return-from gir L))
   (setq b '())
   (loop for square in (pm (car (car L)) (car (cdr (car L))))
-        do (format t "square is: ~S~%" square)
-        (format t "L is: ~S~%" L)
-           (format t "~S~%" (member square L :test 'equal) ) )
+        do (if (not (member square L :test 'equal))
+               (setq b (append b (list (list (car square) (car (cdr square)))))) 
+               )
+        )
+  ;(format t "searchlist: ~S~%" b)
+  (loop for x in b
+        do (gir (cons x L)))
+  (return-from gir nil)
   )
 
-(gir '((0 0) (1 0)))
-
+(print (gir '((0 0)))) 
 
 
 
